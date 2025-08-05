05045f35eea91d0b0de786f330e4814b824e68f1
fabio.maia <fabio.maia@lendico.com.br>
2025-08-05 18:56:57 -0300

    lista dados obtidos via map

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,33 @@ async function getAllPosts() {
     const data = await response.json();
 
     console.log(data);
+
+    //esconde elemento 
+
+    loadingElement.classList.add("hide");
+
+    // percorre todos os elementos do retorno dos dados
+
+    data.map((post) => {
+
+        // criação dos elementos manualmente
+
+        const div = document.createElement("div");
+        const title = document.createElement("h2");
+        const body = document.createElement("p");
+        const link = document.createElement("a");
+
+        title.innerText = post.title;
+        body.innerText = post.body;
+        link.innerText = "Ler";
+        link.setAttribute("href", `/post.html?id=${post.id}`);
+
+        div.appendChild(title);
+        div.appendChild(body);
+        div.appendChild(link);
+        postsContainer.appendChild(div);
+
+    })
     
 }
 
